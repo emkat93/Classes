74b7ab4b861d17c8961f22e37b427bb34700f187
emkat93 <eallen5064@students.mchenry.edu>
2020-09-21 13:19:18 -0500

    added comments to both files

--- src/Employee.kt
+++ src/Employee.kt
@@ -2,9 +2,9 @@ class Employee constructor(var firstName: String,
                            var lastName: String,
                            var employeeNumber: String,
                            var employeeShift: Int
-                           ){
+                           ){  // constructs our class and holds our variables
     fun printAll(){
         println(firstName + " " + lastName + "\nemployee number: "
-                + employeeNumber + "\nshift: " + employeeShift.toString())
+                + employeeNumber + "\nshift: " + employeeShift.toString())  // prints our information in a nice format
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,11 @@
 fun main(){
-    val person1 = Employee("Rosella", "Valentine", "24", 3)
-    val person2 = Employee("Leon", "Pendragon", "16", 1)
-    val person3 = Employee("Julia", "Stone", "60", 2)
+    val person1 = Employee("Rosella", "Valentine", "24", 3)  // person 1 info
+    val person2 = Employee("Leon", "Pendragon", "16", 1)  // person 2 info
+    val person3 = Employee("Julia", "Stone", "60", 2)  // person 3 info
 
-    person1.printAll()
-    println("\n")
-    person2.printAll()
-    println("\n")
-    person3.printAll()
+    person1.printAll() // calls the function printAll to send person 1's info
+    println("\n")  // prints a space between person 1 and 2
+    person2.printAll() // calls the function printAll to send person 2's info
+    println("\n")  // prints a space between person 2 and 3
+    person3.printAll() // calls the function printAll to send person 3's info
 }
